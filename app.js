bbd84b04fcb0f76ddc212ded4bf7010152877a61
Ranveer Singh <ranveer.iiita@gmail.com>
2022-01-17 18:41:34 +0530

    add upload,get and delete file

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs')
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const { uploadFile, getFileStream, deleteFile} = require('./s3')
+
+const app = express();
+
+app.get('/images/:key', (req, res) => {
+    console.log(req.params)
+    const key = req.params.key
+    const readStream = getFileStream(key)
+
+    readStream.pipe(res)
+})
+
+app.post('/images', upload.single('image'), async (req, res) => {
+    const file = req.file
+    console.log(file)
+
+    //image modifications
+    
+    const result = await uploadFile(file)
+    await unlinkFile(file.path)
+    console.log(result)
+    const description = req.body.description
+    res.send({imagePath: `/images/${result.Key}`})
+})
+
+app.delete('/images/:key',(req,res) => {
+    const key = req.params.key;
+    deleteFile(key);
+    return res.status(204).json({
+        success: true,
+        msg: "File Successfully Deleted"
+    })
+})
+
+module.exports = app;
